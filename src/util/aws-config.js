e38ad6eb543187242a654be28c12579994c722c0
izqalan <izqalan@gmail.com>
2022-01-16 15:38:46 +0800

    chore: env name

--- src/util/aws-config.js
+++ src/util/aws-config.js
@@ -1,4 +1,4 @@
 module.exports.collectionName = 'uniten-faces';
 module.exports.region = 'ap-southeast-1';
-module.exports.accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
-module.exports.secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+module.exports.accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID_MYAPP;
+module.exports.secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY_MYAPP;
